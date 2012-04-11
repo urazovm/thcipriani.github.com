60d075087cbebb718abf39ae09986045221e6ad7
Tyler Cipriani <thcipriani@gmail.com>
2012-04-11 14:50:01 -0600

    load

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 /* Author:
-
+  Tyler Cipriani
 */
 
+$(function(){
+  $('a.vcard').on('click',function(e) {
+    e.preventDefault();
+    var $main = $('div.main');
 
+    $main.removeClass('jumbotron').addClass('vcard');
 
-
-
+    $main.load('vcard.html div.main');
+  }
+})();
