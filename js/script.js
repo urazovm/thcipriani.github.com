4c3d1010935fd63d0c2d6e6d37c48e92fce45da0
Tyler Cipriani <thcipriani@gmail.com>
2012-04-11 15:03:10 -0600

    body replace

--- js/script.js
+++ js/script.js
@@ -6,11 +6,8 @@ $(function(){
 
   $('a.vcard').on('click',function(e) {
     e.preventDefault();
-    var $main = $('div.main');
 
-    $main.removeClass('jumbotron').addClass('vcard');
-
-    $main.load('vcard.html div.main');
+    $('body').load('vcard.html div.container');
   });
 
 });
