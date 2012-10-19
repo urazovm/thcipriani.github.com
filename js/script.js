ca2e3c17591234bb74bf07714bf010c7e10460e5
Tyler Cipriani <tyler@tylercipriani.com>
2012-10-19 00:02:54 -0600

    Made fittext.js more explicit

--- js/script.js
+++ js/script.js
@@ -25,5 +25,5 @@ var TylerCipriani = function() {
 
 $(function(){
   TylerCipriani.navScroll();
-  $('.headline').fitText(0.35).lettering();
+  $('section.jumbotron').find('h2.headline').fitText(0.35).lettering();
 });
